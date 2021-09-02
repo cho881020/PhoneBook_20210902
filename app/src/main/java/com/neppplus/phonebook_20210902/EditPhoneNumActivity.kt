2d89b8d3cd17a86a13f3a3bd0433e511f99b0b04
조경진 <cho881020@gmail.com>
2021-09-02 17:09:19 +0900

    폰번 작성 화면으로 이동

---
+++ app/src/main/java/com/neppplus/phonebook_20210902/EditPhoneNumActivity.kt
@@ -0,0 +1,21 @@
+package com.neppplus.phonebook_20210902
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditPhoneNumActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_phone_num)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+}

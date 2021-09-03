4d4bf6e7a377d708e8fc598e016207286b4fc9ea
조경진 <cho881020@gmail.com>
2021-09-03 09:41:55 +0900

    입력값 변수 저장

--- app/src/main/java/com/neppplus/phonebook_20210902/EditPhoneNumActivity.kt
+++ app/src/main/java/com/neppplus/phonebook_20210902/EditPhoneNumActivity.kt
@@ -23,6 +23,24 @@ class EditPhoneNumActivity : BaseActivity() {
 
     override fun setupEvents() {
 
+        okBtn.setOnClickListener {
+
+//            1. 입력한 값들을 변수에 저장
+            val inputName = nameEdt.text.toString()
+            val inputPhoneNum = phoneNumEdt.text.toString()
+
+//            생년월일 (Calendar) -> 1988-05-05   String  => SimpleDateFormat
+
+            val sdf = SimpleDateFormat("yyyy-MM-dd")
+
+            val birthDayStr = sdf.format( mSelectedDate.time )
+
+//            2. 폰번 데이터 객체로 만들자. (클래스 추가)
+
+//            3. 해당 폰번을 -> "이름,폰번,생년월일" 양식으로 가공 -> 파일에 저장.
+
+        }
+
         selectBirthDayBtn.setOnClickListener {
 
 //            달력처럼, 날짜 선택 팝업 출현 (안배운 부분)
